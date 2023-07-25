7f9c59477ca397de3836497d982cd7375a773d94
rocky <cgpa10.0@gmail.com>
2023-07-25 17:24:13 +0530

    project setup - initial commit

---
+++ 1.worlde-game/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "animate.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center text-red-500 animate__animated animate__backInDown">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
