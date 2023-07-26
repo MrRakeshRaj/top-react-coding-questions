8d18f1d65f9c39fe701fa89efb6b0982cd800a61
rocky <cgpa10.0@gmail.com>
2023-07-26 17:31:45 +0530

    refactor code

--- 1.worlde-game/src/components/Board.tsx
+++ 1.worlde-game/src/components/Board.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { words } from "../words";
 import Row from "./Row";
 
+const WORD_LENGTH = 5;
+const NUM_OF_GUESSES = 6;
+
 function Board() {
   const [solution, setSolution] = useState("");
   const [guesses, setGuesses] = useState(Array(6).fill(null));
   const [currentGuess, setCurrentGuess] = useState("");
-  const [isGameOver, setIsGameOver] = useState(false);
+  // const [isGameOver, setIsGameOver] = useState(false);
 
   useEffect(() => {
     const word = words[Math.floor(Math.random() * words.length)];
@@ -15,31 +18,45 @@ function Board() {
   }, []);
 
   useEffect(() => {
-    if (isGameOver) return;
+    if (!solution) return;
 
     const handleType = (e) => {
-      if (e.key === "Enter") {
-        if (currentGuess.length != 5) return;
-
-        const newGuesses = [...guesses];
-        newGuesses[guesses.findIndex((val) => val == null)] = currentGuess;
-        setGuesses(newGuesses);
-        setCurrentGuess("");
-
-        const isCorrect = currentGuess === solution;
-        if (isCorrect) {
-          setIsGameOver(true);
-          return;
-        }
-      }
-
-      if (e.key === "Backspace") {
-        setCurrentGuess(currentGuess.slice(0, -1));
+      if (guesses.includes(solution) || guesses[NUM_OF_GUESSES - 1] != null)
         return;
-      }
 
-      if (currentGuess.length >= 5) return;
-      setCurrentGuess((oldguess) => oldguess + e.key);
+      const charCode = e.key.toLowerCase().charCodeAt(0);
+      const isLetter =
+        e.key.length === 1 &&
+        charCode >= "a".charCodeAt(0) &&
+        charCode <= "z".charCodeAt(0);
+
+      setCurrentGuess((prevGuess) => {
+        if (e.key === "Enter" && prevGuess.length === WORD_LENGTH) {
+          const guessesClone = [...guesses];
+          guessesClone[guesses.findIndex((guess) => guess == null)] = prevGuess;
+          setGuesses(guessesClone);
+          return "";
+        } else if (e.key === "Backspace") {
+          return prevGuess.slice(0, -1);
+        } else if (prevGuess.length < WORD_LENGTH && isLetter) {
+          return prevGuess + e.key.toLowerCase();
+        }
+        return prevGuess;
+      });
+
+      // if (e.key === "Enter") {
+      //   if (currentGuess.length != 5) return;
+
+      //   const newGuesses = [...guesses];
+      //   newGuesses[guesses.findIndex((val) => val == null)] = currentGuess;
+      //   setGuesses(newGuesses);
+      //   setCurrentGuess("");
+
+      //   const isCorrect = currentGuess === solution;
+      //   if (isCorrect) {
+      //     setIsGameOver(true);
+      //     return;
+      //   }
     };
 
     window.addEventListener("keydown", handleType);
@@ -47,7 +64,7 @@ function Board() {
     return () => {
       window.removeEventListener("keydown", handleType);
     };
-  }, [currentGuess, solution, guesses, isGameOver]);
+  }, [solution, guesses]);
 
   return (
     <div className="board flex flex-col gap-3 animate__animated animate__slideInLeft">
